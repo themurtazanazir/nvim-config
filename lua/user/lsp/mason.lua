3d1092e1af66b3b51290a0631ab47d98849864ef
murtaza <themurtazanazir@gmail.com>
2024-10-27 11:43:50 +0530

    better opts and lsp

--- lua/user/lsp/mason.lua
+++ lua/user/lsp/mason.lua
@@ -1,12 +1,6 @@
 local servers = {
-	-- "lua_ls",
-	-- "cssls",
-	-- "html",
-	-- "tsserver",
-	"pyright",
-	-- "bashls",
-	"jsonls",
-	-- "yamlls",
+	"lua_ls",
+	"pylsp",
 }
 
 local settings = {
--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,10 +1,14 @@
 local options = {
-	clipboard = "unnamedplus",
-	number = true,
-	relativenumber = true,
-	cursorline = true,
-	signcolumn = "yes",
-	termguicolors = true, 
+    clipboard = "unnamedplus", 
+    number = true,
+    relativenumber = true,
+    smartindent = true,
+    cursorline = true,
+    signcolumn = "yes",
+    termguicolors = true, 
+    scrolloff = 8,
+    shiftwidth = 4,
+    expandtab = true,
 }
 
 for k, v in pairs(options) do
